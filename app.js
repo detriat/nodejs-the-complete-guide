164775ec605702c74871d85ac2a204fe6afcc396
d.demydenko <d.demydenko@astoundcommerce.com>
2020-04-21 20:19:31 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, './views/errors', '404.html'));
+});
+
+app.listen(3000);
