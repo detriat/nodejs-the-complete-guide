afe9aba054e0d400e41ff746626b076d32750231
d.demydenko <d.demydenko@astoundcommerce.com>
2020-04-23 18:22:08 +0300

    TASK-000:ADD: move to pug

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const express = require('express');
+
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+const products = [];
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, './', 'public')));
 
-app.get('/users', (req, res) => res.sendFile(path.join(__dirname, './views/users', 'index.html')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views', 'home.html')));
-app.use((req, res) => res.status(404).sendFile(path.join(__dirname, './views/errors', '404.html')));
+app.get('/admin/products', (req, res) => res.render('admin/products/index', {
+    pageTitle: 'Add product'
+}));
+
+app.post('/admin/products', (req, res) => {
+    products.push({
+        title: req.body.title
+    });
+    res.redirect('/products');
+});
+
+app.get('/products', (req, res) => res.render('products/index', {
+    pageTitle: 'Products',
+    products
+}));
+
+app.get('/', (req, res) => res.render('home', {
+    pageTitle: 'Home Page'        
+}));
+
+app.use((req, res) => res.render('errors/404', {
+    pageTitle: 'Page not found!'
+}));
 
 app.listen(3000);
